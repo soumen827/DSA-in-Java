235a3122865137a91f81353b492188ba6e78318d
Soumen Laha <soumenlaha465@gmail.com>
2026-08-04 19:55:49 +0530

    Add files via upload

---
+++ Bit Manipulation/BitwiseOr.java
@@ -0,0 +1,14 @@
+public class BitwiseOr{
+    public static void main(String[] args) {
+        int a =5;
+        int b =9;
+        // System.out.println("OR Operation: "+(a|b));
+        // System.out.println("AND Operation: "+(a&b));
+        // System.out.println("XOR Operation: "+(a^b));
+        // System.out.println(Byte.MAX_VALUE);
+        // System.out.println(Short.MAX_VALUE);
+        // System.out.println(Integer.MAX_VALUE);
+        // System.out.println(Long.MAX_VALUE);
+        System.out.println(~89+1);
+    }
+}
---
+++ Bit Manipulation/Left_Right_Shift.java
@@ -0,0 +1,6 @@
+public class Left_Right_Shift {
+    public static void main(String[] args) {
+        System.out.println(91>>2);
+        System.out.println(11<<2);
+    }
+}
