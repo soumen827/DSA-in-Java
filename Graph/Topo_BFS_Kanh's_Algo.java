064e0781f9cccaf81f6b883b708582488aa2acfe
Soumen Laha <soumenlaha465@gmail.com>
2026-09-13 13:20:38 +0530

    Update Topo_BFS_Kanh's_Algo.java

--- Graph/Topo_BFS_Kanh's_Algo.java
+++ Graph/Topo_BFS_Kanh's_Algo.java
@@ -1,6 +1,5 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        
         ArrayList<Integer> ans = new ArrayList<>();
         // Create adjacency list
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -11,26 +10,20 @@ class Solution {
         for(int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
-
+            
             adj.get(u).add(v);
         }
-        
         // Kahn's Algorithm
         int[] indegree = new int[V];
 
         // Calculate indegree
         for(int i = 0; i < V; i++) {
-            for(int ele : adj.get(i)) {
-                indegree[ele]++;
-            }
+            for(int ele : adj.get(i)) indegree[ele]++;
         }
         Queue<Integer> q = new LinkedList<>();
-
         // Add all indegree 0 nodes
         for(int i = 0; i < V; i++) {
-            if(indegree[i] == 0) {
-                q.add(i);
-            }
+            if(indegree[i] == 0) q.add(i);
         }
         // BFS
         while(!q.isEmpty()) {
@@ -38,11 +31,12 @@ class Solution {
             ans.add(node);
             for(int ele : adj.get(node)) {
                 indegree[ele]--;
-                if(indegree[ele] == 0) {
-                    q.add(ele);
-                }
+                if(indegree[ele] == 0) q.add(ele);
             }
         }
         return ans;
+    }
+}
+
     }
 }
